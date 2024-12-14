98d8dbd9b8c19c22e324f36db5e27703b475543d
oleg.nazarenko.dev <oleg.nazarenko.dev@gmail.com>
2024-12-14 19:02:34 +0300

    Add SumBusinessLogicTest.

---
+++ Tests/RepeatableCallTracer.Tests/InMemoryCallTraceWriter.cs
@@ -0,0 +1,14 @@
+﻿namespace RepeatableCallTracer.Tests;
+
+public partial class SumBusinessLogicTest
+{
+    internal sealed class InMemoryCallTraceWriter : ICallTraceWriter
+    {
+        public LinkedList<CallTrace> Traces { get; } = [];
+
+        public void Append(CallTrace trace)
+        {
+            Traces.AddLast(trace);
+        }
+    }
+}
---
+++ Tests/RepeatableCallTracer.Tests/SumBusinessLogicTest.cs
@@ -0,0 +1,69 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+using RepeatableCallTracer.Debuggers;
+using RepeatableCallTracer.Targets;
+
+namespace RepeatableCallTracer.Tests;
+
+public partial class SumBusinessLogicTest
+{
+    internal interface ISumBusinessLogic
+    {
+        int Calculate(int a, int b);
+    }
+
+    internal sealed class SumBusinessLogic : ISumBusinessLogic
+    {
+        public int Calculate(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    internal sealed class SumBusinessLogicTracer(
+        SumBusinessLogic target,
+        ICallTraceWriter callTraceWriter,
+        IDebugCallTraceProvider debugCallTraceProvider)
+        : TracedTarget<ISumBusinessLogic>(target, callTraceWriter, debugCallTraceProvider, new CallTracerOptions()), ISumBusinessLogic
+    {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public int Calculate(int a, int b)
+        {
+            using var scope = BeginOperation(MethodBase.GetCurrentMethod()!);
+
+            a = scope.SetParameter(nameof(a), a);
+            b = scope.SetParameter(nameof(b), b);
+
+            return Target.Calculate(a, b);
+        }
+    }
+
+    [Fact]
+    public void Test_PrimitiveTypesArgs_WithoutExternalDependencies_Successful()
+    {
+        DebugCallTraceProvider debugCallTraceProvider = new();
+        InMemoryCallTraceWriter callTraceWriter = new();
+        
+        SumBusinessLogic target = new();
+        SumBusinessLogicTracer tracer = new(target, callTraceWriter, debugCallTraceProvider);
+
+        var expectedA = 1;
+        var expectedB = 2;
+
+        var actualForwardCallResult = tracer.Calculate(expectedA, expectedB);
+        Assert.Single(callTraceWriter.Traces);
+
+        var trace = callTraceWriter.Traces.First!.Value;
+        debugCallTraceProvider.EnableDebug(typeof(ISumBusinessLogic), trace);
+
+        var actualDebugCallResult = tracer.Calculate(0, 0);
+        Assert.Equal(actualForwardCallResult, actualDebugCallResult);
+
+        var actualA = trace.GetTargetMethodParameter<int>("a");
+        Assert.Equal(actualA, expectedA);
+
+        var actualB = trace.GetTargetMethodParameter<int>("b");
+        Assert.Equal(actualB, expectedB);
+    }
+}
